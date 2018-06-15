7ef976786fb916a7b4cd9fa5a5e53199fca88ef8
dani <dani.zaghian@gmail.com>
2018-06-14 18:24:08 -0700

    added subtotal tax and total calculations

--- app/controllers/api/orders_controller.rb
+++ app/controllers/api/orders_controller.rb
@@ -6,16 +6,29 @@ class Api::OrdersController < ApplicationController
 	end
 
 	def create
-	  @order = Order.new(
+		#subtotal = products price * order qty
+
+	  @order = Order.create(
 	   user_id: current_user.id,
 	   product_id: params[:product_id],
-	   quantity: params[:quantity],
-	   subtotal: params[:subtotal],
-	   tax: params[:tax],
-	   total: params[:total]
+	   quantity: params[:quantity]
 	  )
 
-	  @order.save
+	  calculated_subtotal = @order.product.price * @order.quantity.to_i
+	  calculated_tax = calculated_subtotal * 0.09
+	  calulated_total = calculated_subtotal + calculated_tax
+
+	  @order.update(
+	  	subtotal: calculated_subtotal, 
+	  	tax: calculated_tax, 
+	  	total: calulated_total
+	  )
+
 	  render 'show.json.jbuilder'
 	end
 end
+
+
+
+
+
