0079805b44966bd9f7281f5fee7faf5f463289ef
dani <dani.zaghian@gmail.com>
2018-06-21 21:50:31 -0700

    adding validations and happy/sad paths

--- app/controllers/api/products_controller.rb
+++ app/controllers/api/products_controller.rb
@@ -36,8 +36,11 @@ class Api::ProductsController < ApplicationController
       supplier_id: params[:supplier_id]
     )
 
-    @product.save
-    render 'show.json.jbuilder'
+    if @product.save
+      render 'show.json.jbuilder'
+    else
+      render json: {errors: @product.errors.full_messages}, status: :unprocessable_entity
+    end
   end
 
   def update
--- app/models/product.rb
+++ app/models/product.rb
@@ -9,6 +9,13 @@ class Product < ApplicationRecord
 	has_many :carted_products
 	has_many :orders, through: :carted_products
 
+	validates :name, presence: true
+	validates :name, uniqueness: true
+	validates :price, presence: true
+	validates :price, numericality: { greater_than: 0 }
+	validates :description, presence: true
+	validates :description, length: { in: 10..500 }
+
 	def is_discounted?
 		price < 10
 	end
